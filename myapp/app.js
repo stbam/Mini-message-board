12b06220cce61ac6c472ef139e944d4cd480a26c
stbam <55661516+stbam@users.noreply.github.com>
2023-12-26 18:07:34 -0500

    mongodb update

--- myapp/app.js
+++ myapp/app.js
@@ -1,21 +1,27 @@
 var createError = require('http-errors');
 var express = require('express');
+const mongoose = require("mongoose");
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 const Author  = require('./models/author');
+const bodyParser = require('body-parser');
+
 
 port = 3000
 
-var indexRouter = require('./routes/index');
+/*var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var secondIndexRouter= require('./routes/secondindex')
+*/
+//var secondIndexRouter= require('./routes/secondindex')
+
 var app = express();
 
-const mongoose = require("mongoose");
+
 mongoose.set("strictQuery",false);
-const mongoDB = "mongodb+srv://stbacmtd:Stru5932@cluster0.qoxy26k.mongodb.net/?retryWrites=true&w=majority";
+const mongoDB =  "mongodb+srv://stbacmtd:Stru5932@cluster0.qoxy26k.mongodb.net/?retryWrites=true&w=majority";
+
+
 
 main().catch((err)=>console.log(err));
 async function main(){
@@ -28,15 +34,38 @@ async function main(){
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+/*app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/secondindex',secondIndexRouter);
+*/
+//app.use('/secondindex',secondIndexRouter);
+
+app.get('/', async (req, res) => {
+  
+  const authors = await fetchAuthors();
+  
+  res.render('index2', {authors});
+});
+
+/*
+
+app.get('/',async(req,res)=>{
+  const authors = await fetchAuthors();
+
+  res.render('secondindex',{authors});
+}
+)*/
+
+
 
 app.post('/addAuthor', async (req, res) => {
   const { first_name, family_name, other_fields } = req.body;
@@ -72,5 +101,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+async function fetchAuthors() {
+  return await Author.find().exec();
+}
 
 module.exports = app;
