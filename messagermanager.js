4c625a006252d5eeb7dd71d7bb77084cdc5e727b
stbam <55661516+stbam@users.noreply.github.com>
2023-12-27 18:20:28 -0500

    update

--- messagermanager.js
+++ messagermanager.js
@@ -2,14 +2,20 @@
 const Message = require('./models/messagemodel');
 require('./app.js'); // Ensure the database connection is established
 
+const MAX_RETRIES = 3;
+let retryCount = 0;
+
 const getMessages = async () => {
-  try {
-    const messages = await Message.find().lean();
-    return messages || [];
-  } catch (error) {
-    console.error('Error fetching messages:', error);
-    throw error;
+  while (retryCount < MAX_RETRIES) {
+    try {
+      const messages = await Message.find().lean().exec();
+      return messages || [];
+    } catch (error) {
+      console.error('Error fetching messages:', error);
+      retryCount++;
+    }
   }
+  throw new Error('Max retries reached. Unable to fetch messages.');
 };
 
 
